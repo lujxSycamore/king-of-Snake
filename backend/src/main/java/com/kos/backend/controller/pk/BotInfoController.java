5dce47c5eef386512dbabcaaa70afb034b6a330f
lujxSycamore <158274498@qq.com>
2023-01-13 15:54:48 +0800

    前后端连接

--- backend/src/main/java/com/kos/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kos/backend/controller/pk/BotInfoController.java
@@ -3,11 +3,25 @@ package com.kos.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("botInfo/")
-    public String getBotInfo(){
-        return "test";
+    public List getBotInfo(){
+        List<Map<String,String>> list = new ArrayList();
+        Map<String,String> bot1 = new HashMap<>();
+        bot1.put("name","snake1");
+        bot1.put("rank","1500");
+        Map<String,String> bot2 = new HashMap<>();
+        bot2.put("name","dog");
+        bot2.put("rank","648");
+        list.add(bot1);
+        list.add(bot2);
+        return list;
     }
 }
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div class="app">
-    hello world
-  </div>
+
+  <div>name: {{bot_name}}</div>
+  <div>rank: {{bot_rank}}</div>
+  <router-view></router-view>
 </template>
+<script>
+import { ref } from "vue" //vue创建变量需要引入
+import $ from "jquery" //$代表jQuery 回传函数
+
+export default {
+  name: "APP",
+  setup(){
+    let bot_name = ref("");
+    let bot_rank = ref("");
+    $.ajax({
+      url: "http://localhost:3000/pk/botInfo/", //url只能小写,需要解决跨域问题
+      type: "get",
+      success: response =>{
+        console.log(response);
+        bot_name.value = response[0].name;
+        bot_rank.value = response[0].rank;
+      }
+    })
+    return{
+      bot_name,
+      bot_rank
+    }
+  }
+}
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body{
+  background-image: url("./assets/background.png");
+  background-size: cover;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
